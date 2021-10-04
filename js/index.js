0221fe57eb13641ee4ba02e188d97526671a0a80
EddyPERREUL <e.perreul@gmail.com>
2021-10-04 10:34:55 +0200

    error resolve nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 "use strict";
+
+// nav Hamburger
+
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
+
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
